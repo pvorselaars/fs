a99c2c21868ec24dd1bc5e344403818bfcccb2ff
Perry Vorselaars <p.vorselaars@gmail.com>
2024-11-23 15:35:18 +0100

    Add flags

--- fat.c
+++ fat.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -37,9 +38,13 @@ typedef struct {
 	uint8_t filename[8];
 	uint8_t extension[3];
 	uint8_t attributes;
-	uint8_t reserved[10];
-	uint16_t time;
-	uint16_t date;
+	uint8_t reserved[2];
+	uint16_t create_time;
+	uint16_t create_date;
+	uint16_t last_access_date;
+	uint8_t ignored[2];
+	uint16_t last_write_time;
+	uint16_t last_write_date;
 	uint16_t cluster;
 	uint32_t size;
 } root_entry_t;
@@ -51,6 +56,13 @@ bpb16_t *get_bpb16(FILE * disk)
 	fseek(disk, 0, SEEK_SET);
 	if (!fread(bpb, sizeof(*bpb), 1, disk)) {
 		fprintf(stderr, "%s", strerror(errno));
+		free(bpb);
+		return NULL;
+	}
+
+	if (bpb->signature != 0xaa55) {
+		fprintf(stderr, "Invalid boot sector signature\n");
+		free(bpb);
 		return NULL;
 	}
 
@@ -125,7 +137,7 @@ int display_bpb16(bpb16_t * bpb)
 	}
 	printf("\n");
 
-	printf("Signature:\t\t0x%04X\n\n", bpb->signature);
+	printf("Signature:\t\t0x%04X\n", bpb->signature);
 
 	return 0;
 }
@@ -184,7 +196,6 @@ void display_fat12(fat12_t * fat, bpb16_t * bpb)
 		}
 	}
 	printf("\n");
-	printf("\n");
 }
 
 root_entry_t *get_rootdir(FILE * disk, bpb16_t * bpb)
@@ -205,74 +216,90 @@ root_entry_t *get_rootdir(FILE * disk, bpb16_t * bpb)
 	return rootdir;
 }
 
-void display_rootdir(root_entry_t * rootdir, bpb16_t * bpb)
+void get_fullname(char *buffer, const root_entry_t * entry)
 {
+	char name[9], ext[4];
+	name[8] = '\0';
+	ext[3] = '\0';
+	buffer[0] = '\0';
+
+	memcpy(name, entry->filename, 8);
+	memcpy(ext, entry->extension, 3);
+
+	// null terminate space padded strings
+	for (int c = 7; c >= 0; c--) {
+		if (name[c] == ' ') {
+			name[c] = '\0';
+		} else {
+			break;
+		}
+	}
+
+	for (int c = 2; c >= 0; c--) {
+		if (ext[c] == ' ') {
+			ext[c] = '\0';
+		} else {
+			break;
+		}
+	}
+
+	if (ext[0] != '\0') {
+		strcat(buffer, name);
+		strcat(buffer, ".");
+		strcat(buffer, ext);
+	} else {
+		strcat(buffer, name);
+	}
+}
+
+void display_rootdir(char details, root_entry_t * rootdir, bpb16_t * bpb)
+{
+	char name[13];
+	uint8_t hours, minutes, dseconds, month, day;
+	uint16_t year;
+
 	// Print all valid entries
-	printf("Root directory entries:\n");
 	for (int i = 0; i < bpb->root_entries; i++) {
 
 		if (rootdir[i].filename[0] == 0)
 			continue;
 
 		// Print filename and extension
-		for (int c = 0; c < 8; c++) {
-			printf("%c", rootdir[i].filename[c]);
+		get_fullname(name, &rootdir[i]);
+
+		if (details) {
+			day = rootdir[i].create_date & 0x1f;
+			month = (rootdir[i].create_date >> 5) & 0xf;
+			year = ((rootdir[i].create_date >> 9) & 0x7f) + 1980;
+
+			hours = (rootdir[i].create_time >> 11) & 0x1f;
+			minutes = (rootdir[i].create_time >> 5) & 0x0f;
+			dseconds = (rootdir[i].create_time & 0x1f) * 2;
+			printf("%-12s %16d B %02d:%02d:%02d %d-%d-%d\n", name, rootdir[i].size, hours, minutes,
+			       dseconds, day, month, year);
+		} else {
+			printf("%-12s\n", name);
 		}
-		printf(".");
-		for (int c = 0; c < 3 && rootdir[i].extension[c] != 0; c++) {
-			printf("%c", rootdir[i].extension[c]);
-		}
-		printf("\t%dB @%d\n", rootdir[i].size, rootdir[i].cluster);
 	}
 }
 
 void get_file(const char *filename, FILE * disk, root_entry_t * rootdir, fat12_t * fat, bpb16_t * bpb)
 {
-	char name[9], ext[4], fullname[13];
+	char name[13];
 
 	for (int i = 0; i < bpb->root_entries; i++) {
 		if (rootdir[i].filename[0] == 0)
 			continue;
 
-		name[8] = '\0';
-		ext[3] = '\0';
-		fullname[0] = '\0';
+		get_fullname(name, &rootdir[i]);
 
-		memcpy(name, rootdir[i].filename, 8);
-		memcpy(ext, rootdir[i].extension, 3);
-
-		// null terminate space padded strings
-		for (int c = 7; c >= 0; c--) {
-			if (name[c] == ' ') {
-				name[c] = '\0';
-			} else {
-				break;
-			}
-		}
-
-		for (int c = 2; c >= 0; c--) {
-			if (ext[c] == ' ') {
-				ext[c] = '\0';
-			} else {
-				break;
-			}
-		}
-
-		if (ext[0] != '\0') {
-			strcat(fullname, name);
-			strcat(fullname, ".");
-			strcat(fullname, ext);
-		} else {
-			strcat(fullname, name);
-		}
-
-		if (strcmp(fullname, filename) == 0) {
+		if (strcmp(name, filename) == 0) {
 			char *buffer = (char *)malloc(bpb->sectors_per_cluster * bpb->bytes_per_sector);
 
 			uint32_t offset;
 			uint16_t cluster = rootdir[i].cluster;
 
-			while (cluster < 0xff6) {
+			while (cluster < 0xff6 && cluster != 0) {
 
 				offset = cluster_to_address(cluster, bpb);
 				fseek(disk, offset, SEEK_SET);
@@ -288,29 +315,86 @@ void get_file(const char *filename, FILE * disk, root_entry_t * rootdir, fat12_t
 	}
 }
 
+void usage(char *bin)
+{
+	printf("Usage: %s [options] <disk>\n", bin);
+	printf("\n");
+	printf("Options:\n");
+	printf("  -h		show this help message and exit\n");
+	printf("  -l		list files\n");
+	printf("  -d		list file details\n");
+	printf("  -b		show boot parameter block\n");
+	printf("  -f		show FAT\n");
+}
+
 int main(int argc, char *argv[])
 {
+	char *filename;
+	char list_flag, bpb_flag, fat_flag, details_flag = 0;
+	int c;
 
-	const char *filename = "disk.img";
+	while ((c = getopt(argc, argv, "hldbgf")) != -1) {
+		switch (c) {
+		case 'l':
+			list_flag = 1;
+			break;
+
+		case 'd':
+			details_flag = 1;
+			break;
+
+		case 'b':
+			bpb_flag = 1;
+			break;
+
+		case 'f':
+			fat_flag = 1;
+			break;
+
+		case 'h':
+			usage(argv[0]);
+			return 0;
+			break;
+
+		case '?':
+			usage(argv[0]);
+			return 1;
+			break;
+		}
+	}
+
+	if (optind < argc) {
+		filename = argv[optind];
+	} else {
+		usage(argv[0]);
+		return 1;
+	}
+
 	FILE *disk = fopen(filename, "rb");
 
 	bpb16_t *bpb = get_bpb16(disk);
 	if (bpb == NULL)
 		return -1;
 
-	display_bpb16(bpb);
+	if (bpb_flag) {
+		display_bpb16(bpb);
+	}
 
 	fat12_t *fat = get_fat12(disk, bpb);
 	if (fat == NULL)
 		return -1;
 
-	display_fat12(fat, bpb);
+	if (fat_flag) {
+		display_fat12(fat, bpb);
+	}
 
 	root_entry_t *rootdir = get_rootdir(disk, bpb);
 	if (rootdir == NULL)
 		return -1;
 
-	display_rootdir(rootdir, bpb);
+	if (list_flag) {
+		display_rootdir(details_flag, rootdir, bpb);
+	}
 
 	fclose(disk);
 
